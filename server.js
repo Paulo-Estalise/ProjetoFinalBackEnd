d9c276569ce62032939d5336d9cf6160dc2530f6
Paulo-Estalise <phestalise@gmail.com>
2024-10-07 20:52:14 -0300

    correção aula30

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const productRoutes = require('routes/products'); // Certifique-se de que o caminho está correto
+const productRoutes = require('./models/Product')
 
 const app = express();
 const PORT = 8080;
 
-// Conectar ao MongoDB
-mongoose.connect('mongodb://localhost:27017/seuBancoDeDados', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('MongoDB conectado!');
-})
-.catch(err => {
-    console.error('Erro ao conectar ao MongoDB:', err);
-});
+mongoose.connect('mongodb://localhost:27017/seuBancoDeDados')
+    .then(() => {
+        console.log('MongoDB conectado!');
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao MongoDB:', err);
+    });
 
-// Middleware para permitir o uso de JSON nas requisições
 app.use(express.json());
 
-// Definindo as rotas de produtos
 app.use('/api/products', productRoutes);
 
-// Iniciando o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
